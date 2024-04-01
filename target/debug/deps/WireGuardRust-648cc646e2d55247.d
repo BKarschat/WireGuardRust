f59cadd9f9792d8df0996a834167f46f91b4e83d
Bastian Karschat <Bastian.Karschat@gmail.com>
2024-04-01 20:40:54 +0200

    Init repo with cargo new

---
+++ target/debug/deps/WireGuardRust-648cc646e2d55247.d
@@ -0,0 +1,9 @@
+/home/ann0s/Documents/WireGuardRust/WireGuardRust/target/debug/deps/libWireGuardRust-648cc646e2d55247.rmeta: src/main.rs Cargo.toml
+
+/home/ann0s/Documents/WireGuardRust/WireGuardRust/target/debug/deps/WireGuardRust-648cc646e2d55247.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/WireGuardRust-b0743edb697a0c25.d
@@ -0,0 +1,9 @@
+/home/ann0s/Documents/WireGuardRust/WireGuardRust/target/debug/deps/libWireGuardRust-b0743edb697a0c25.rmeta: src/main.rs Cargo.toml
+
+/home/ann0s/Documents/WireGuardRust/WireGuardRust/target/debug/deps/WireGuardRust-b0743edb697a0c25.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
